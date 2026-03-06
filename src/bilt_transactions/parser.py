5829803f9c24780345344fec9caec3a08735ae98
John Conery <jconery@icloud.com>
2026-03-06 10:17:33 -0800

    Add parser, unit tests for parser

--- src/bilt_transactions/parser.py
+++ src/bilt_transactions/parser.py
@@ -6,6 +6,8 @@
 import re
 from unmhtml import MHTMLConverter
 
+from bilt_transactions.token import Token
+
 def extract_paragraphs(fn: str):
     '''
     Return the contents of all the <P> elements in an MHTML file.
@@ -17,4 +19,91 @@ def extract_paragraphs(fn: str):
     html = converter.convert_file(fn)
     return re.findall(r'<p class.*?</p>', html)
 
+
+def parse_file(fn: str):
+    '''
+    Parse an MHTML file, returning a list of dicts, where each dict
+    describes a single transaction.  Attributes are date, description,
+    and amount.
+
+    The parser calls "extract_paragraphs" to get a list of text elements
+    in the file.  It then uses a finite state machine (see "states.dot" 
+    in the documentation) to create output records.  The main loop 
+    removes a paragraph from the list, creates a token, and then moves
+    to the state defined by token type. 
+
+    Arguments:
+        fn:  the name of the file to parse
+    '''
+
+    def step(token, state):
+        match state:
+            case 'A':
+                clear_transaction()
+                if token.kind == 'date':
+                    consume(token, 'A -> D')
+                    next = 'D'
+                else:
+                    missing_edge(token, 'A')
+                    next = 'A'
+            case 'D':
+                if token.kind == 'description':
+                    consume(token, 'D -> X')
+                    next = 'X'
+                else:
+                    missing_edge(token, 'D')
+                    next = 'A'
+            case 'X':
+                if token.kind == 'amount':
+                    consume(token, 'X -> N')
+                    next = 'N'
+                else:
+                    missing_edge(token, 'X')
+                    next = 'A'
+            case 'N':
+                save_transaction()
+                if token.kind == 'date':
+                    consume(token, 'N -> D')
+                    next = 'D'
+                elif token.kind == 'description':
+                    consume(token, 'N -> X')
+                    next = 'X'
+                else:
+                    missing_edge(token, 'N')
+                    next = 'A'
+        return next
+    
+    def consume(token, msg):
+        transaction[token.kind] = token.value
+        print(f'{msg}: {token.kind} = {token.value}')
+
+    def missing_edge(token, state):
+        printf('error: state {state} has no action for {token.kind}')
+    
+    def clear_transaction():
+        for k in transaction.keys():
+            transaction[k] = None
+
+    def save_transaction():
+        output.append(dict(transaction))
+
+    paragraphs = extract_paragraphs(fn)
+    state = 'A'
+    output = []
+
+    transaction = {
+        'date': None,
+        'description': None,
+        'amount': None
+    }
+
+    while paragraphs:
+        p = paragraphs.pop(0)
+        if t := Token(p):
+            state = step(t, state)
+        else:
+            print(f'error making token for {p}')
+    
+    save_transaction()
+    return output
 
--- tests/test_bilt_transactions.py
+++ tests/test_bilt_transactions.py
@@ -1,4 +1,4 @@
-from bilt_transactions.parser import extract_paragraphs
+from bilt_transactions.parser import extract_paragraphs, parse_file
 from bilt_transactions.token import Token
 
 def test_paragraphs():
@@ -25,4 +25,16 @@ def test_amount_token():
     assert t.kind == 'amount'
     assert round(t.value, 2) == 13.20
 
-
+def test_parser():
+    lst = parse_file('tests/feb.mhtml')
+    assert len(lst) == 3
+    assert [isinstance(x, dict) for x in lst]
+    assert lst[0]['date'].month == 2
+    assert lst[0]['date'].day == 9
+    assert lst[0]['date'].year == 2026
+    assert lst[0]['description'] == 'Best Buy'
+    assert lst[-1]['date'].month == 2
+    assert lst[-1]['date'].day == 8
+    assert lst[-1]['date'].year == 2026
+    assert lst[-1]['description'] == 'Art House'
+    assert round(sum(x['amount'] for x in lst),2) == 81.45 
