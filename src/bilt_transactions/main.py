d667045e1c20d4c1d0a30f5fa673fed13d960200
John Conery <jconery@icloud.com>
2026-03-06 11:30:09 -0800

    swtich build system to Hatch, add main.py (command line top level)

---
+++ src/bilt_transactions/main.py
@@ -0,0 +1,11 @@
+#
+# bilt_transactions
+#
+# A command line application, written in Python, that extracts transactions
+# from a MHTML file downloaded from bilt.com and prints them in CSV format.
+#
+
+def main():
+    print('bilt_transactions')
+
+
